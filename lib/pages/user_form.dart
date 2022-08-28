010f330a4377b32f371b000af49e3cf9c47dfe8f
Adriel <adrielsilva_1990@hotmail.com>
2022-08-28 15:56:59 -0300

    implementando os campos de texto do formulário, com validação

--- lib/pages/user_form.dart
+++ lib/pages/user_form.dart
@@ -3,7 +3,10 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class UserForm extends StatelessWidget {
-  const UserForm({Key? key}) : super(key: key);
+  UserForm({Key? key}) : super(key: key);
+
+    final _form = GlobalKey<FormState>(); 
+    final Map<String, String> _formData = {};
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +15,49 @@ class UserForm extends StatelessWidget {
         title: Text('Formulário de Usuários'),
         actions: [
           IconButton(onPressed: () {
+            final isValid = _form.currentState?.validate();
+
+            if(isValid == true && isValid != null) {
+            _form.currentState?.save();
             Navigator.of(context).pop();
+            } 
           },
-          icon: Icon(Icons.save_alt_outlined)),
-        ],
+          icon: Icon(Icons.save_alt_outlined),
+          ),
+        ], 
+        ),
+        body: Padding(padding: EdgeInsets.all(18),
+        child: Form(
+          key: _form,
+          child: Column(
+            children: [
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Nome'),
+              validator: (value) {
+                return "Ocorreu um erro";
+              },
+              onSaved: (value){
+                print(value);
+              }
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Data de Nascimento'),
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Número de Telefone'),
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Como gosta de ser chamado'),
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'AvatarUrl'),
+            ),
+        ]
+        ,),
+        ),
         ),
       );
   }
