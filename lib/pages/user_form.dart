ce5c4af2baa56d47aa9c2c3cb0157ca49afdbdd2
Adriel <adrielsilva_1990@hotmail.com>
2022-08-28 22:52:32 -0300

    configurando os métodos de validação e inclusão

--- lib/pages/user_form.dart
+++ lib/pages/user_form.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_application_desafio/classes/classUsers.dart';
+import 'package:flutter_application_desafio/provider/users.dart';
+import 'package:provider/provider.dart';
+
 
 class UserForm extends StatelessWidget {
   UserForm({Key? key}) : super(key: key);
@@ -8,21 +10,39 @@ class UserForm extends StatelessWidget {
     final _form = GlobalKey<FormState>(); 
     final Map<String, String> _formData = {};
 
+
   @override
   Widget build(BuildContext context) {
+
+    final user = ModalRoute.of(context)!.settings.arguments;
+
+      
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Formulário de Usuários'),
+        title: Text('Formulário de Usuário'),
         actions: [
-          IconButton(onPressed: () {
-            final isValid = _form.currentState?.validate();
+          IconButton(icon: Icon(Icons.save_alt_outlined),
+            onPressed: () {
+                final isValid = _form.currentState!.validate();
 
-            if(isValid == true && isValid != null) {
-            _form.currentState?.save();
-            Navigator.of(context).pop();
-            } 
-          },
-          icon: Icon(Icons.save_alt_outlined),
+
+              if(isValid){
+              _form.currentState!.save();
+
+              Provider.of<UsersProvider>(context, listen: false).put(User(id: _formData['id']!,
+              nome: _formData['nome']!, 
+              dataNascimento: _formData['dataNascimento']!, 
+              apelido: _formData['apelido']!, 
+              email: _formData['email']!, 
+              celular: _formData['celular']!, 
+              avatarUrl: _formData['avatarUrl!']!,
+              ),
+              );
+              Navigator.of(context).pop();
+              }
+            
+            },         
           ),
         ], 
         ),
@@ -34,26 +54,40 @@ class UserForm extends StatelessWidget {
             TextFormField(
               decoration: InputDecoration(labelText: 'Nome'),
               validator: (value) {
-                return "Ocorreu um erro";
+                if(value == null || value.isEmpty){
+                return 'Nome inválido.';
+                }
+                if(value.trim().length < 3){
+                  return 'Insira um nome no mínimo com 3 letras.';
+                }
+
+                return null;
               },
-              onSaved: (value){
-                print(value);
-              }
-            ),
+              onSaved: (value)=> _formData['nome'] = value!,
+            ), 
             TextFormField(
               decoration: InputDecoration(labelText: 'Email'),
+              onSaved: (value)=> _formData['email'] = value!,
             ),
             TextFormField(
               decoration: InputDecoration(labelText: 'Data de Nascimento'),
+              onSaved: (value)=> _formData['dataNascimento'] = value!,
+
             ),
             TextFormField(
               decoration: InputDecoration(labelText: 'Número de Telefone'),
+              onSaved: (value)=> _formData['celular'] = value!,
+
             ),
             TextFormField(
               decoration: InputDecoration(labelText: 'Como gosta de ser chamado'),
+              onSaved: (value)=> _formData['apelido'] = value!,
+
             ),
             TextFormField(
               decoration: InputDecoration(labelText: 'AvatarUrl'),
+              onSaved: (value)=> _formData['avatarUrl'] = value!,
+
             ),
         ]
         ,),
